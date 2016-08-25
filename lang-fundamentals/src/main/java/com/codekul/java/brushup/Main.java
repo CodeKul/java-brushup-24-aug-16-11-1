ac15f011ab539500ae83306e2fe599802bcf06e7
aniruddha <aniruddha.kudalkar@gmail.com>
2016-08-25 14:25:24 +0530

    initial commit

---
+++ lang-fundamentals/src/main/java/com/codekul/java/brushup/Main.java
@@ -0,0 +1,10 @@
+package com.codekul.java.brushup;
+
+/**
+ * Created by aniruddha on 25/8/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World .. Gradle ...");
+    }
+}
